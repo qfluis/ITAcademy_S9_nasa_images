07f42913941ca9d0b71f6bb68e1ddcc742ddec6f
Luis Quevedo <qfluis@gmail.com>
2022-05-22 23:22:36 +0200

    cambios en frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendAuth: "http://localhost:4000/api/auth",
-  backendFav: "http://localhost:4000/api/like"
+  backendAuth: "https://nasapics-qfluis.herokuapp.com/api/auth",//"http://localhost:4000/api/auth",
+  backendFav: "https://nasapics-qfluis.herokuapp.com/api/like"//"http://localhost:4000/api/like"
 };
